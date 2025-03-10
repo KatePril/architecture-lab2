2eeb8d282469f747a90bebd1b66306cefb77b0a5
Kateryna Prylutska <prylutska.kateryna@lll.kpi.ua>
2025-03-10 15:40:11 +0200

    Update ComputeHandler and Compute method

--- handler.go
+++ handler.go
@@ -1,12 +1,30 @@
 package lab2
 
+import (
+	"bufio"
+	"fmt"
+	"io"
+)
+
 // ComputeHandler should be constructed with input io.Reader and output io.Writer.
 // Its Compute() method should read the expression from input and write the computed result to the output.
 type ComputeHandler struct {
-	// TODO: Add necessary fields.
+	input  io.Reader
+	output io.Writer
 }
 
 func (ch *ComputeHandler) Compute() error {
-	// TODO: Implement.
-	return nil
+	scanner := bufio.NewScanner(ch.input)
+	if !scanner.Scan() {
+		return fmt.Errorf("Input Error")
+	}
+
+	expression := scanner.Text()
+	transformedExpression, err1 := PrefixToInfix(expression)
+	if err1 != nil {
+		return err1
+	}
+
+	_, err2 := fmt.Fprint(ch.output, transformedExpression)
+	return err2
 }
